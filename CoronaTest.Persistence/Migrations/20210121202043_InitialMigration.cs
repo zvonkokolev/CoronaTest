46c04f260f21b48e1785363dc0d5fdd79868405e
Name <Name@DESKTOP-6ADNBQH>
2021-01-21 21:23:03 +0100

    DB initialisation

---
+++ CoronaTest.Persistence/Migrations/20210121202043_InitialMigration.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CoronaTest.Persistence.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "VerificationTokens",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    RowVersion = table.Column<byte[]>(rowVersion: true, nullable: true),
+                    Token = table.Column<int>(nullable: false),
+                    Identifier = table.Column<Guid>(nullable: false),
+                    IssuedAt = table.Column<DateTime>(nullable: false),
+                    IsInvalidated = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_VerificationTokens", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "VerificationTokens");
+        }
+    }
+}
